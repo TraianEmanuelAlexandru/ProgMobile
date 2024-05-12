60b935fcc22f08c665053219244ac4cea3d7dee4
TraianEmanuel <81159716+TraianEmanuel@users.noreply.github.com>
2024-05-12 18:25:37 +0200

    signup

--- MyGym/app/src/main/java/com/example/mygym/ui/signup/SignupFragment.kt
+++ MyGym/app/src/main/java/com/example/mygym/ui/signup/SignupFragment.kt
@@ -1,19 +1,23 @@
 package com.example.mygym.ui.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.mygym.databinding.FragmentHomeAdminBinding
-import com.example.mygym.ui.home.HomeAdminViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.fragment.app.FragmentResultListener
+import com.example.mygym.LoginActivity
+import com.example.mygym.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+
 
 class SignupFragment : Fragment() {
-    private var _binding: FragmentHomeAdminBinding? = null
-
+    private var _binding: FragmentSignupBinding? = null
+    private lateinit var firebaseAuth: FirebaseAuth
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,13 +26,45 @@ class SignupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-
-        _binding = FragmentHomeAdminBinding.inflate(inflater, container, false)
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signupButton.setOnClickListener{
+            val email = binding.signupEmail.text.toString()
+            val password = binding.signupPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() ) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "Signup successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(context, LoginActivity::class.java)
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(context, "Signup unsuccessful", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(context, "Enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.loginRedirect.setOnClickListener{
+            startActivity(Intent(context, LoginActivity::class.java))
+        }
+    }
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -36,3 +72,5 @@ class SignupFragment : Fragment() {
     }
 
 }
+
+
---
+++ MyGym/app/src/main/java/com/example/mygym/ui/signup/SignupViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.mygym.ui.signup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import com.example.mygym.LoginActivity
+import com.example.mygym.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignupViewModel : ViewModel() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
+
+
+
+
+
+
+
+}
