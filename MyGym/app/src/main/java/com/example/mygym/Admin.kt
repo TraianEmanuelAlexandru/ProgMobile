bce7d71e4a47a035f42afc1e8bbfe5afa03fe9fd
TraianEmanuel <81159716+TraianEmanuel@users.noreply.github.com>
2024-05-12 18:32:32 +0200

    signup

--- MyGym/app/src/main/java/com/example/mygym/Admin.kt
+++ MyGym/app/src/main/java/com/example/mygym/Admin.kt
@@ -3,5 +3,5 @@ package com.example.mygym
 data class Admin(val username: String, val email: String)
 val adminsList = mutableListOf(
     Admin("Emanuel", "emanuel.traian@gmail.com"),
-    Admin("Enzo", "enzocingoli@gmail.com")
+    Admin("Enzo", "enzo.cingoli@gmail.com")
 )
